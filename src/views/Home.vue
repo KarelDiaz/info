ee31c08791c72069c5151d7bac9d07f975e0a397
Karel Diaz <kdiaz@theinit.com>
2021-10-29 11:10:18 -0400

    Adding new works

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,7 +19,7 @@ import PersonalComponent from "../components/PersonalComponent.vue";
 export default {
   components: {
     HistoryComponent,
-    PersonalComponent,
+    PersonalComponent
   },
   data() {
     return {
@@ -30,37 +30,79 @@ export default {
           {
             name: "Telegram",
             icon: "telegram",
-            link: "https://t.me/KarelDiazAlfonso",
+            link: "https://t.me/KarelDiazAlfonso"
           },
           {
             name: "Twitter",
             icon: "twitter",
-            link: "https://twitter.com/KarelDiazA",
+            link: "https://twitter.com/KarelDiazA"
           },
           {
             name: "LinkedIn",
             icon: "linkedin",
-            link: "https://linkedin.com/KarelDiaz",
+            link: "https://linkedin.com/KarelDiaz"
           },
           {
             name: "GitHub",
             icon: "github",
-            link: "https://github.com/KarelDiaz",
-          },
+            link: "https://github.com/KarelDiaz"
+          }
         ],
         skillsFrontend: [
           { tecName: "Vue", stars: 5 },
           { tecName: "Angular", stars: 4 },
-          { tecName: "Tailwind", stars: 5 },
+          { tecName: "Tailwind", stars: 5 }
         ],
         skillsBackend: [
           { tecName: "Express", stars: 5 },
-          { tecName: "Strapi", stars: 5 },
+          { tecName: "Strapi", stars: 5 }
         ],
         histories: [
           {
             name: "Work",
             contents: [
+              {
+                name: "Colaboracion Ciudadana",
+                name2: "Sadeco",
+                dateStart: "01-10-2021",
+                dateEnd: "01-11-2021",
+                description:
+                  "In the application it is possible share information about lost pets or animal with other user and get reward for that.",
+                events: [],
+                role: "Frontend Developer",
+                team: "3 Developers",
+                tasks: ["Mockup views"],
+                technologies: ["Angular", "Ionic"],
+                links: [
+                  {
+                    title: "Sadeco here",
+                    link: "https://www.sadeco.es/"
+                  },
+                ]
+              },
+              {
+                name: "Caser Starbien",
+                name2: "Theinit",
+                dateStart: "01-09-2021",
+                dateEnd: "01-11-2021",
+                description:
+                  "Is a private project to help people how are inscribed in Caser Seguros to tracking his exercises.",
+                events: [],
+                role: "Frontend Developer",
+                team: "4 Developers",
+                tasks: [],
+                technologies: ["Vuejs", "Quasarjs"],
+                links: [
+                  {
+                    title: "Theinit here",
+                    link: "https://www.theinit.com"
+                  },
+                  {
+                    title: "Caser Seguros here",
+                    link: "https://www.caser.es"
+                  }
+                ]
+              },
               {
                 name: "Binary Coffee",
                 name2: "Open Source Projects",
@@ -76,18 +118,18 @@ export default {
                   "Angular",
                   "Angular Universal",
                   "Strapi",
-                  "MongoDB",
+                  "MongoDB"
                 ],
                 links: [
                   {
                     title: "You can find it in github here",
-                    link: "https://github.com/orgs/dcs-community",
+                    link: "https://github.com/orgs/dcs-community"
                   },
                   {
                     title: "This is the oficial page of Binary Coffee",
-                    link: "https://binary-coffee.dev",
-                  },
-                ],
+                    link: "https://binary-coffee.dev"
+                  }
+                ]
               },
               {
                 name: "School timetable",
@@ -101,18 +143,19 @@ export default {
                 team: "1 Developer",
                 tasks: [
                   "Implement the timetable module for public information ",
-                  "Implement the manage view for professors",
+                  "Implement the manage view for professors"
                 ],
                 technologies: ["Vue", "Tailwind", "Strapi", "MongoDB"],
                 links: [
                   {
                     title: "You can look at the university here",
-                    link: "https://cuaje.edu.cu",
-                  },
-                ],
+                    link: "https://cuaje.edu.cu"
+                  }
+                ]
               },
               {
-                name: "Post-processing module to illustrative visualization of volumes using GPU",
+                name:
+                  "Post-processing module to illustrative visualization of volumes using GPU",
                 name2: "Thesis, Graduation paper",
                 dateStart: "2017",
                 dateEnd: "2018",
@@ -123,17 +166,17 @@ export default {
                 team: "1 Developer",
                 tasks: [
                   "Implement the plugin to integrate with Vismedic Illutration",
-                  "Implement the each of the efects using GPU",
+                  "Implement the each of the efects using GPU"
                 ],
                 technologies: ["Qt", "C++ (CPU)", "GLSL (GPU)"],
                 links: [
                   {
                     title: "You can look at the university here",
-                    link: "https://uci.cu",
-                  },
-                ],
-              },
-            ],
+                    link: "https://uci.cu"
+                  }
+                ]
+              }
+            ]
           },
           {
             name: "Education",
@@ -147,7 +190,7 @@ export default {
                 events: [
                   { name: "ICPC Caribbean Finals", date: "2015/11" },
                   { name: "ICPC Caribbean Finals", date: "2017/11" },
-                  { name: "Global Game Jam", date: "2018" },
+                  { name: "Global Game Jam", date: "2018" }
                 ],
                 role: "",
                 team: "",
@@ -156,15 +199,15 @@ export default {
                 links: [
                   {
                     title: "You can look at the university site",
-                    link: "https://uci.cu",
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-      },
+                    link: "https://uci.cu"
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
     };
-  },
+  }
 };
 </script>
